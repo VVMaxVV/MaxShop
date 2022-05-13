aca230a2d82ab8efafe91badaf57a03ad9955230
VVMaxVV <maksimvoloshko02@gmail.com>
2022-05-13 23:57:00 +0300

    Included navigation library in project

--- app/src/main/java/com/maxshop/ui/MainActivity.kt
+++ app/src/main/java/com/maxshop/ui/MainActivity.kt
@@ -3,7 +3,6 @@ package com.maxshop.ui
 import android.os.Bundle
 import com.example.maxshop.R
 import com.example.maxshop.databinding.ActivityMainBinding
-import com.maxshop.fragment.CategoriesFragment
 import dagger.android.support.DaggerAppCompatActivity
 
 class MainActivity : DaggerAppCompatActivity() {
@@ -13,8 +12,5 @@ class MainActivity : DaggerAppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        binding?.fragmentContainer?.let {
-            supportFragmentManager.beginTransaction().replace(it.id, CategoriesFragment()).commit()
-        }
     }
 }
